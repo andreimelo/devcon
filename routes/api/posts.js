5e56d2ad40848ff4da0ae5879ba9e970919f519a
andreimelo <meloandrei69@gmail.com>
2020-09-28 23:18:39 +0800

    Added space

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // @route  GET api/posts
 // @desc   Test route 
 // @access Public
+
 router.get('/', (req, res) => res.send("Posts Route"));
 
 module.exports = router;
